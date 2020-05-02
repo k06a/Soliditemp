57890c82ef4d9b981519db74dcc047e115e660c9
Anton Bukov <k06aaa@gmail.com>
2020-05-02 23:38:25 +0300

    Add example

--- contracts/Soliditemp.sol
+++ contracts/Soliditemp.sol
@@ -4,4 +4,9 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 
 contract Soliditemp is Ownable {
+    event Log(uint256 a);
+
+    function func(uint256 a) public {
+        emit Log(a);
+    }
 }
--- test/Soliditemp.js
+++ test/Soliditemp.js
@@ -6,7 +6,8 @@ const Soliditemp = artifacts.require('Soliditemp');
 contract('Soliditemp', function ([_, addr1]) {
     describe('Soliditemp', async function () {
         it('should be ok', async function () {
-            this.token = await Soliditemp.new();
+            this.contract = await Soliditemp.new();
+            await this.contract.func(1);
         });
     });
 });
